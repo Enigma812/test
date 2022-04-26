a93641e26210ec30a56eda0bf68963e483b639fb
Enigma812 <antiseptic812@gmail.com>
2022-04-26 22:10:04 +0300

    feat (add): active button eng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Cat } from '../models/cat';
+import { Buttonhome } from '../models/buttonhome';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,11 @@ export class AppComponent {
   public name = 'test';
   public cat: Cat;
   public cat2: Cat;
+  public rus: Buttonhome;
 
   constructor() {
       this.cat = new Cat('Мурка');
       this.cat2 = new Cat('Маруся');
+      this.rus = new Buttonhome();
   }
 }
---
+++ src/models/buttonhome.ts
@@ -0,0 +1,11 @@
+export class Buttonhome {
+    public home: string;
+
+    constructor () {
+        this.home = "Главная";
+    }
+
+    public translate() {
+        this.home = "Home";
+    }
+}
