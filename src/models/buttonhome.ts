c3661fff8ecfb972ec3e9d0be44bb3b089226468
Enigma812 <antiseptic812@gmail.com>
2022-04-27 17:02:45 +0300

    feat(bt): add rus eng buttins

--- src/models/buttonhome.ts
+++ src/models/buttonhome.ts
@@ -1,11 +1,23 @@
 export class Buttonhome {
     public home: string;
+    public menu: string;
+    public bookmark: string;
 
     constructor () {
         this.home = "Главная";
+        this.menu = "Меню";
+        this.bookmark = "Закладки"
     }
 
     public translate() {
         this.home = "Home";
+        this.menu = "Menu";
+        this.bookmark = "Bookmarks";
+    }
+
+    public retranslate () {
+        this.home = "Главная";
+        this.menu = "Меню";
+        this.bookmark = "Закладки"
     }
 }
